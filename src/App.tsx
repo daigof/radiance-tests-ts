ab6742854da154fbe3ca4ed1813f38e2612b9270
Diego Fortes <daigof@gmail.com>
2020-10-22 13:31:50 -0300

    update dependeciees

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
 import React from "react";
-// import ConstantsComp from "./components/constantsComp";
+import ConstantsComp from "./components/constantsComp";
+import TooltipComp from "./components/tooltipComp";
+// import AccordionComp from "./components/accordionComp";
 // import RoundButtonComp from "./components/roundButtonComp";
-import LinkButtonComp from "./components/linkButtonComp";
+// import LinkButtonComp from "./components/linkButtonComp";
 // import TextButtonComp from "./components/textButtonComp";
 // import ButtonComp from "./components/buttonComp";
-// import AccordionComp from "./components/accordionComp";
 // import CheckboxComp from "./components/checkboxComp";
 // import DialogModalComp from "./components/dialogModalComp";
 // import ImmersiveModalComp from "./components/immersiveModalComp";
-// import TooltipComp from "./components/tooltipComp";
 import { Global } from "@emotion/core";
 
 import {
   brandStyles,
   resetStyles,
 } from "radiance-ui/lib/utils/injectGlobalStyles/style";
+// import { primaryTheme, secondaryTheme } from "radiance-ui/lib/constants/themes";
+// import { ThemeProvider } from "emotion-theming";
 
 const App: React.FC = () => {
+  // console.log(primaryTheme);
   return (
     <div>
       <Global styles={resetStyles} />
       <Global styles={brandStyles} />
-      {/* <ConstantsComp /> */}
-      <LinkButtonComp />
+      <ConstantsComp />
+      <TooltipComp />
+      {/* <LinkButtonComp /> */}
       {/* <TextButtonComp /> */}
       {/* <RoundButtonComp /> */}
       {/* <ButtonComp /> */}
-      {/* <AccordionComp />
-      <CheckboxComp />
-      <DialogModalComp />
-      <ImmersiveModalComp />
-      <TooltipComp /> */}
+      {/* <AccordionComp /> */}
+      {/* <CheckboxComp />*/}
+      {/* <DialogModalComp />*/}
+      {/* <ImmersiveModalComp />*/}
     </div>
   );
 };
